a8e8590adee9272edf4101c821316881d6d86259
John Cañarte <jecanart@espol.edu.ec>
2023-06-29 22:43:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ let load = (data) => {
 }
 
 let loadInocar = () => {
-  let URL_proxy = 'http://cors-anywhere.herokuapp.com/';
+  let URL_proxy = 'https://cors-anywhere.herokuapp.com/';
   let URL = URL_proxy + 'https://www.inocar.mil.ec/mareas/consultan.php';
   fetch(URL)
      	.then(response => response.text())
